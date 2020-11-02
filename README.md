148bcbb119a0705d40b2d721a6dc63c42a00e4db
Matvii Hodovaniuk <matvii@hodovani.uk>
2020-11-02 14:41:01 +0200

    Add google auth

---
+++ README.md
@@ -0,0 +1,1 @@
+1. Put Google `clientID` and `clientSecret` in `config/keys.js`;
---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+	google: {
+		clientID: "330914956096-q5tng78bm1pn1veco90hnuhpll5jl00k.apps.googleusercontent.com",
+		clientSecret: "VAD-EVKcw9qjn6ILx-oiLK_7"
+	}
+}
